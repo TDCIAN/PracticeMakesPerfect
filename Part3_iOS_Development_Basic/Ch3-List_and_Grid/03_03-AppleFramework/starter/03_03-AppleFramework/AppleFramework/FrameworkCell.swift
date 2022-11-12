757159ae97d8aa94418303d51eb51113cf96e5f6
TDCIAN <tdcian71@gmail.com>
2022-11-12 14:54:51 +0900

    made apple framework

---
+++ Part3_iOS_Development_Basic/Ch3-List_and_Grid/03_03-AppleFramework/starter/03_03-AppleFramework/AppleFramework/FrameworkCell.swift
@@ -0,0 +1,23 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework
+//
+//  Created by JeongminKim on 2022/11/12.
+//
+
+import UIKit
+
+class FrameworkCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameLabel.numberOfLines = 1
+        nameLabel.adjustsFontSizeToFitWidth = true
+    }
+    func configure(_ framework: AppleFramework) {
+        thumbnailImageView.image = UIImage(named: framework.imageName)
+        nameLabel.text = framework.name
+    }
+}
