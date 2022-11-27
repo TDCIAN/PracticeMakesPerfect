ee58606469bf9ea27808186a82d7f6ffcad9598a
TDCIAN <tdcian71@gmail.com>
2022-11-27 23:58:48 +0900

    add benefitlistviewcontroller

---
+++ Part8_Projects/01-Toss_Benefits/starter/TossBenefitTab/TossBenefitTab/BenefitListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BenefitListViewController.swift
+//  TossBenefitTab
+//
+//  Created by JeongminKim on 2022/11/27.
+//
+
+import UIKit
+
+class BenefitListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
