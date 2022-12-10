f6e9d59db2defa8afe433d91b3a7670109f25fbf
TDCIAN <tdcian71@gmail.com>
2022-12-10 15:16:37 +0900

    config base diary ui

--- Part8_Projects/03-Emotion_Diary/starter/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
+++ Part8_Projects/03-Emotion_Diary/starter/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct DiaryListView: View {
-    var body: some View {
-        Text("Hello, Emotion Diary World")
-            .font(.system(size: 24, weight: .bold))
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        DiaryListView()
+    
+    @StateObject var vm: DiaryListViewModel
+    
+    let layout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    var body: some View {        
+        LazyVGrid(columns: layout) {
+            ForEach(vm.keys, id: \.self) { key in
+                Section {
+                    let items = vm.dic[key] ?? []
+                    let orderedItems = items.sorted { $0.date < $1.date }
+                    
+                    ForEach(orderedItems) { item in
+                        MoodDiaryCell(diary: item)
+                            .frame(height: 50)
+                    }
+                } header: {
+                    Text(key)
+                }
+                
+            }
+            .onAppear {
+                print("앱 켜짐")
+            }
+        }
     }
 }
