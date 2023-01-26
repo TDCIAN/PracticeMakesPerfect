413b067bdf30719513bd3c26010639b3e98c7538
TDCIAN <tdcian71@gmail.com>
2023-01-26 21:10:38 +0900

    use traits

--- RxSwiftPractice/RxSwiftPractice/Traits.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice/Traits.playground/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+import RxSwift
+import Foundation
 
-var greeting = "Hello, playground"
+let disposeBag = DisposeBag()
+
+struct SomeJSON: Decodable {
+    let name: String
+}
+
+enum JSONError: Error {
+    case decodingError
+}
+
+let json1 = """
+    {"name":"park"}
+    """
+
+let json2 = """
+    {"my_name":"kim"}
+    """
+
+func decode(json: String) -> Single<SomeJSON> {
+    Single<SomeJSON>.create { observer -> Disposable in
+        guard let data = json.data(using: .utf8),
+              let json = try? JSONDecoder().decode(SomeJSON.self, from: data) else {
+                  observer(.failure(JSONError.decodingError))
+                  return Disposables.create()
+              }
+        observer(.success(json))
+        return Disposables.create()
+    }
+}
+
+decode(json: json1)
+    .subscribe {
+        switch $0 {
+        case .success(let json):
+            print("성공 - \(json)")
+        case .failure(let error):
+            print("실패 - \(error)")
+        }
+        
+    }.disposed(by: disposeBag)
