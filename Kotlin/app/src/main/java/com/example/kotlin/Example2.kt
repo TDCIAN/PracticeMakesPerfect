5e8ab195721e4657bf29124ff45d2586b6084ffa
TDCIAN <tdcian71@gmail.com>
2023-02-09 22:25:30 +0900

    data class and sealed class

--- Kotlin/app/src/main/java/com/example/kotlin/Example2.kt
+++ Kotlin/app/src/main/java/com/example/kotlin/Example2.kt
@@ -1,13 +1,32 @@
 package com.example.kotlin
 
-lateinit var text: String
-val test : Int by lazy {
-    println("초기화 중")
-    100
-}
 fun main() {
-    println("메인 함수 실행")
-    println("초기화한 값 $test")
-    println("두번째 호출 $test")
+    val dog = Dog(name = "해피", age = 24)
 
+    println(dog.toString())
+    println(dog.copy(age = 5))
+
+    val cat: Cat = BlueCat()
+    val result = when(cat) {
+        is BlueCat -> "blue"
+        is RedCat -> "red"
+        is GreenCat -> "green"
+        else -> "none"
+    }
+
+    println(result)
 }
+
+data class Dog(
+    val name: String,
+    val age: Int
+) {
+    override fun toString(): String {
+        return "직접 구현 ${name}는 ${age}살임"
+    }
+}
+
+sealed class Cat
+class BlueCat: Cat()
+class RedCat: Cat()
+class GreenCat: Cat()
