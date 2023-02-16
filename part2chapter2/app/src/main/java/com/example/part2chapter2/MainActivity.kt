2eea5584a61145f1483f74cb495b83590ba34cd6
TDCIAN <tdcian71@gmail.com>
2023-02-16 22:04:40 +0900

    add permission code

--- part2chapter2/app/src/main/java/com/example/part2chapter2/MainActivity.kt
+++ part2chapter2/app/src/main/java/com/example/part2chapter2/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.part2chapter2
 
+import android.Manifest.permission.RECORD_AUDIO
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.part2chapter2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_CODE = 200
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,6 +20,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.recordButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // You can use the API that requires the permission.
+                }
+                shouldShowRequestPermissionRationale(...) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+                showInContextUI(...)
+            }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissions(this,
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        REQUEST_RECORD_AUDIO_CODE)
+
+                }
+            }
+
+        }
 
     }
 }
