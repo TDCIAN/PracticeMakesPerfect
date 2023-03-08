1141747a697ce65b31703dac00bdd3920663f5f2
TDCIAN <tdcian71@gmail.com>
2023-03-08 21:21:52 +0900

    config section and list

--- starter/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
+++ starter/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
@@ -19,21 +19,69 @@ struct DiaryListView: View {
         GridItem(.flexible()),
     ]
     
-    var body: some View {        
-        LazyVGrid(columns: layout) {
-            ForEach(vm.keys, id: \.self) { key in
-                Section {
-                    let items = vm.dic[key] ?? []
-                    let orderedItems = items.sorted { $0.date < $1.date }
-                    ForEach(orderedItems) { item in
-                        MoodDiaryCell(diary: item)
-                            .frame(height: 50)
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: layout) {
+                        ForEach(vm.keys, id: \.self) { key in
+                            Section {
+                                let items = vm.dic[key] ?? []
+                                let orderedItems = items.sorted { $0.date < $1.date }
+                                ForEach(orderedItems) { item in
+                                    MoodDiaryCell(diary: item)
+                                        .frame(height: 50)
+                                }
+                            } header: {
+                                Text(formattedSectionTitle(key))
+                                    .font(.system(size: 30, weight: .bold))
+                            }
+                            .frame(height: 60)
+                            .padding()
+                        }
                     }
-                } header: {
-                    Text(key)
                 }
                 
-            }
+                HStack {
+                    Button {
+                        print("New Button Tapped")
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                    }
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(40)
+                }
+            }.navigationTitle("Emotion Diary")
+            
         }
+    }
+}
+
+extension DiaryListView {
+    // 2022-4 -> April 2022
+    private func formattedSectionTitle(_ id: String) -> String {
+        let dateComponents = id
+            .components(separatedBy: "-")
+            .compactMap { Int($0) }
+        guard let year = dateComponents.first,
+              let month = dateComponents.last else {
+                  return id
+              }
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponent = DateComponents(
+            calendar: calendar,
+            year: year,
+            month: month
+        )
+        let date = dateComponent.date!
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        return formatter.string(from: date)
     }
 }
