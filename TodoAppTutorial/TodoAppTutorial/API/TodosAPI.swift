59d3c6c9dba75175ad5addbbd365cd0d98c3b1dc
TDCIAN <tdcian71@gmail.com>
2023-04-16 22:02:09 +0900

    handle error

--- TodoAppTutorial/TodoAppTutorial/API/TodosAPI.swift
+++ TodoAppTutorial/TodoAppTutorial/API/TodosAPI.swift
@@ -19,10 +19,28 @@ enum TodosAPI {
 
     enum ApiError: Error {
         case badUrl
-        case parsingError
-        case noContentError
+        case noContent
         case decodingError
-        case basStatus(code: Int)
+        case unauthorized
+        case badStatus(code: Int)
+        case unknown(_ err: Error?)
+        
+        var info: String {
+            switch self {
+            case .badUrl:
+                return "잘못된 URL입니다."
+            case .noContent:
+                return "데이터가 없습니다."
+            case .decodingError:
+                return "디코딩 에러입니다."
+            case .unauthorized:
+                return "인증되지 않은 사용자입니다."
+            case .badStatus(code: let code):
+                return "에러 상태코드: \(code)"
+            case .unknown(let err):
+                return "알 수 없는 에러입니다. \(err)"
+            }
+        }
     }
     
     static func fetchTodos(
@@ -39,20 +57,44 @@ enum TodosAPI {
         urlRequest.httpMethod = "GET"
         urlRequest.addValue("application/json", forHTTPHeaderField: "accept")
         
-        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+        URLSession.shared.dataTask(with: urlRequest) { data, urlResponse, err in
+            
+            if let error = err {
+                print(#fileID, #function, #line, "- request error: \(err)")
+                return completion(.failure(ApiError.unknown(error)))
+            }
+            
+            guard let httpResponse = urlResponse as? HTTPURLResponse else {
+                print(#fileID, #function, #line, "- bad status code")
+                return completion(.failure(ApiError.unknown(nil)))
+            }
+            
+            switch httpResponse.statusCode {
+            case 401:
+                return completion(.failure(ApiError.unauthorized))
+            default:
+                print(#fileID, #function, #line, "- code: \(httpResponse.statusCode)")
+            }
+            
+            if !(200...299).contains(httpResponse.statusCode) {
+                return completion(.failure(ApiError.badStatus(code: httpResponse.statusCode)))
+            }
+            
             if let jsonData = data {
                 do {
                     let todosResponse = try JSONDecoder().decode(TodosResponse.self, from: jsonData)
-                    let modelObjects = todosResponse.data
-                    print(#fileID, #function, #line, "- modelObjects: \(modelObjects)")
+                    let todos = todosResponse.data
+                    
+                    guard let todos = todos,
+                          !todos.isEmpty else {
+                        return completion(.failure(ApiError.noContent))
+                    }
+                    
                     completion(.success(todosResponse))
                 } catch(let error) {
                     print(#fileID, #function, #line, "- catch error: \(error)")
                     completion(.failure(.decodingError))
                 }
-            } else {
-                print(#fileID, #function, #line, "- request error: \(error)")
-                completion(.failure(.noContentError))
             }
         }.resume()
     }
--- TodoAppTutorial/TodoAppTutorial/TodosVM.swift
+++ TodoAppTutorial/TodoAppTutorial/TodosVM.swift
@@ -12,14 +12,35 @@ class TodosVM: ObservableObject {
     
     init() {        
         print(#fileID, #function, #line, "- ")
-        TodosAPI.fetchTodos { result in
+        TodosAPI.fetchTodos { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case .success(let todosResponse):
                 print(#fileID, #function, #line, "- vm success: \(todosResponse)")
             case .failure(let error):                
                 print(#fileID, #function, #line, "- vm error: \(error)")
+                self.handleError(error)
             }
         }
     }
     
+    fileprivate func handleError(_ err: Error) {
+        if err is TodosAPI.ApiError {
+            let apiError = err as! TodosAPI.ApiError
+            switch apiError {
+            case .badUrl:
+                print("배드유알엘")
+            case .noContent:
+                print("컨텐츠 없음")
+            case .decodingError:
+                print("디코딩 안 됨")
+            case .unauthorized:
+                print("인증 안 됨")
+            case .badStatus(code: let code):
+                print("배드 스테이터스 - 코드: \(code)")
+            case .unknown(let error):
+                print("알 수 없음: \(error)")
+            }
+        }
+    }
 }
